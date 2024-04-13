14bb73f8a68609c283185f3dfd03bb9b9919dfb1
Gede Prasidha Bhawarnawa <gede.prasidha@gmail.com>
2024-04-13 20:10:16 +0700

    update: bisa generate nilai N

--- problemB/problemB.py
+++ problemB/problemB.py
@@ -1,12 +1,12 @@
 from sympy import nextprime
 import socket
-import sys, time
+import time
+from math import gcd
 
 ip = '165.232.161.196'
 port = 1303
 token = 'vyGHUZR7Sb'
 
-max_wait_time = 10 # dalam detik
 error_message = b'Terjadi kesalahan'
 
 # Butuh paling tidak (hipotesis) 10 pasangan ciphertext dan plaintext untuk memastikan jawabannya benar
@@ -30,34 +30,57 @@ while True:
     data = sk.recv(1024 * 100)
 
     # Ambil 10 pasangan ciphertext dan plaintext
-    if len(cm_pairs) < 10:
-        print("Start isi data")
-        print(data.decode())
-        print("End isi data")
-        print("Nilai input saat ini: {}".format(input) )
-        print("Panjang cm_pairs saat ini: {}".format(len(cm_pairs)))
+    if len(cm_pairs) < 20:
         if b'Masukkan perintah:' in data:
             sk.send(b'1\n')
         elif b'Masukkan nomor arsip (dalam bentuk integer):' in data and not has_sent_input:
             sk.send(str(input).encode() + b'\n')
             has_sent_input = True
         elif b'Masukkan isi arsip:' in data:
-            print("Sedang mengirim input {}".format(input))
+            # print("Sedang mengirim input {}".format(input))
             sk.send(b'Hai\n')
         elif b'Token akses nomor arsip:' in data:
-            print("Mau parsing token arsip")
+            # print("Mau parsing token arsip")
             # Parse output
             token = data.decode().split(': ')[1].strip()
-            print('Untuk input {}, token = {}'.format(input, token))
+            # print('Untuk input {}, token = {}'.format(input, token))
             cm_pairs.append([input, int(token)])
-            print("Selesai untuk input {}".format(input))
+            # print("Selesai untuk input {}".format(input))
             has_sent_input = False
             input += 1
         elif error_message in data:
             print('Terjadi kesalahan')
             break
     else:
-        print('Selesai mengambil 10 pasangan ciphertext dan plaintext')
+        print('Selesai mengambil 20 pasangan ciphertext dan plaintext')
         break
 
-    print("Isi cm_pairs saat ini: {}".format(cm_pairs))
+print("Isi cm_pairs saat ini: {}".format(cm_pairs))
+
+# Cari nilai e dengan cara bruteforce dari 2^15 sampai 2^16
+gcd_options = []
+for e in range(2**15, 2**16):
+    # Alasan kenapa e harus ganjil adalah karena harus koprima dengan phi (hasil kali (p-1)(q-1))
+    # Karena p dan q adalah bilangan prima, maka p-1 dan q-1 adalah bilangan genap
+    # Maka dari itu, phi adalah bilangan genap, sehingga e harus ganjil
+    if e % 2 == 0:
+        continue
+
+    calculation_results = [] # Berisikan selisih antara C dengan M^e
+
+    for cm_pair in cm_pairs:
+        c = cm_pair[1]
+        m = cm_pair[0]
+        calculation_results.append(c - m**e)
+    
+    gcd_val = calculation_results[0]
+    for result in calculation_results:
+        gcd_val = gcd(gcd_val, result)
+    
+    if gcd_val != 1:
+        gcd_options.append([e, gcd_val])
+
+print("Hasil perhitungan (Kemungkinan Nilai N):")
+print(gcd_options)
+
+
